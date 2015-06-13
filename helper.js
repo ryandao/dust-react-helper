653fcf925b417f9382ca09a8e11848a4556ac3a5
Ken Wronkiewicz <wh@wirewd.com>
2015-06-13 09:38:03 -0700

    15380% performance improvement using memoization

--- helper.js
+++ helper.js
@@ -6,19 +6,30 @@ require('node-jsx').install();
 
 var reactDir;
 
+var memoize = require('memoizee');
+
+var getComponentFactory = memoize(function getComponentFactory(file) {
+  var component;
+
+  if (reactDir) {
+    component = require(path.resolve(path.join(reactDir, file)));
+  } else {
+    component = require(path.resolve(file));
+  }
+  return React.createFactory(component);
+});
+
+var renderComponent = memoize(function renderComponent(factory, props) {
+   return React.renderToString(factory(props))
+}, { max: 2000 })
+
 module.exports = {
   install: function(dust) {
     dust.helpers.react = function(chunk, context, bodies, params) {
       var file = params.component;
-      var component;
-
-      if (reactDir) {
-        component = require(path.resolve(path.join(reactDir, file)));
-      } else {
-        component = require(path.resolve(file));
-      }
-
-      var markup = React.renderToString(React.createElement(component));
+      var props = params.props;
+      var factory = getComponentFactory(file);
+      var markup = renderComponent(factory, props);
       chunk.write(markup);
       return chunk;
     };
